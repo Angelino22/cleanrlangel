1db008089f841d63ed5dfa8e3089b033af4bc1e1
Angel Valencia <angelrvo@hotmail.com>
2024-07-14 20:38:31 +0200

    Update Validation_2.py

--- cleanrl/Validation_2.py
+++ cleanrl/Validation_2.py
@@ -13,22 +13,61 @@ import tyro
 from stable_baselines3.common.buffers import ReplayBuffer
 
 
+@dataclass
 class Args:
-    env_id: str = "foozpong_v3"
-    exp_name: str = "experiment_2"
+    exp_name: str = os.path.basename(__file__)[: -len(".py")]
+    """the name of this experiment"""
     seed: int = 1
-    buffer_size: int = 100000
-    start_e: float = 1.0
-    end_e: float = 0.05
-    exploration_fraction: float = 0.1
-    total_timesteps: int = 100000
-    capture_video: bool = False
-    track: bool = False
-    save_path: str = "models/q_network.pth"
-    wandb_project_name: str = "test_project"
-    wandb_entity: str = "test_entity"
+    """seed of the experiment"""
     torch_deterministic: bool = True
-    cuda: bool = False
+    """if toggled, `torch.backends.cudnn.deterministic=False`"""
+    cuda: bool = True
+    """if toggled, cuda will be enabled by default"""
+    track: bool = False
+    """if toggled, this experiment will be tracked with Weights and Biases"""
+    wandb_project_name: str = "cleanRL"
+    """the wandb's project name"""
+    wandb_entity: str = None
+    """the entity (team) of wandb's project"""
+    capture_video: bool = True
+    """whether to capture videos of the agent performances (check out `videos` folder)"""
+    save_model: bool = False
+    """whether to save model into the `runs/{run_name}` folder"""
+    upload_model: bool = False
+    """whether to upload the saved model to huggingface"""
+    hf_entity: str = ""
+    """the user or org name of the model repository from the Hugging Face Hub"""
+
+    # Algorithm specific arguments
+    env_id: str = "foozpong_v3"
+    """the id of the environment"""
+    total_timesteps: int = 10000000
+    """total timesteps of the experiments"""
+    learning_rate: float = 1e-4
+    """the learning rate of the optimizer"""
+    num_envs: int = 1
+    """the number of parallel game environments"""
+    buffer_size: int = 1000000
+    """the replay memory buffer size"""
+    gamma: float = 0.99
+    """the discount factor gamma"""
+    tau: float = 1.0
+    """the target network update rate"""
+    target_network_frequency: int = 1000
+    """the timesteps it takes to update the target network"""
+    batch_size: int = 32
+    """the batch size of sample from the replay memory"""
+    start_e: float = 0.1
+    """the starting epsilon for exploration"""
+    end_e: float = 0.01
+    """the ending epsilon for exploration"""
+    exploration_fraction: float = 0.10
+    """the fraction of `total-timesteps` it takes from start-e to go end-e"""
+    learning_starts: int = 50000
+    """timestep to start learning"""
+    train_frequency: int = 4
+    """the frequency of training"""
+
 
 
 def make_env(env_id, seed, capture_video, run_name):
